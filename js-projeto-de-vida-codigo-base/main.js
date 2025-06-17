b0cc6fe1e7f7a6efafdd5268aca3e783960dfd21
Danilo <117854044+Danilo7712n@users.noreply.github.com>
2025-06-17 13:49:15 -0300

    Add files via upload

--- js-projeto-de-vida-codigo-base/main.js
+++ js-projeto-de-vida-codigo-base/main.js
@@ -15,12 +15,22 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll (".contador");
-let tempoAtual = new Date ();
-const TempoObjetivo1 = new Date ("2026-03-27T00:00:00") - tempoAtual;
-contadores[0].textContent = TempoObjetivo1;
-const TempoObjetivo2 = new Date ("2027-04-05T00:00:00") - tempoAtual;
-contadores[1].textContent = TempoObjetivo2;
-const TempoObjetivo3 = new Date ("2025-09-30T00:00:00") - tempoAtual;
-contadores[2].textContent = TempoObjetivo3;
-const TempoObjetivo4 = new Date ("2028-03-02T00:00:00") - tempoAtual;
-contadores[3].textContent = TempoObjetivo4;
+
+const TempoObjetivo1 = new Date ("2026-03-27T00:00:00");
+contadores[0].textContent = calculaTempo (TempoObjetivo1);
+const TempoObjetivo2 = new Date ("2027-04-05T00:00:00");
+contadores[1].textContent = calculaTempo (TempoObjetivo2);
+const TempoObjetivo3 = new Date ("2025-09-30T00:00:00");
+contadores[2].textContent = calculaTempo (TempoObjetivo3);
+const TempoObjetivo4 = new Date ("2028-03-02T00:00:00");
+contadores[3].textContent = calculaTempo (TempoObjetivo4);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return dias;
+}
